50f74d7b2e25f0113da6c5a946faac71f0005e83
faraon4i <web@eventbritge.ru>
2025-01-03 14:45:34 +0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-
-# Install OpenVPN
-apt-get update;
-apt-get install -y curl;
-curl -O https://raw.githubusercontent.com/faraon4i/support/main/ovpn
-mv ovpn vpn
-mv vpn /bin
-echo "_______________________________________"
-echo "Vpn script baslatmak ucin vpn yazyn"
