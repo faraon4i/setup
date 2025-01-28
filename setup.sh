92a785beab436a959917d0458801497f9f266bc4
faraon4i <web@eventbritge.ru>
2025-01-28 20:04:16 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-
 # Install OpenVPN
-
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m" && Green="\033[32m" && Red="\033[31m" && Yellow="\033[33m" && Blue='\033[34m' && Purple='\033[35m' && Ocean='\033[36m' && Black='\033[37m' && Morg="\033[5m" && Reverse="\033[7m" && Font="\033[1m"
 sh_ver="7.7.7"
 Error="${Red_background_prefix}[Ошибка]${Font_color_suffix}"
